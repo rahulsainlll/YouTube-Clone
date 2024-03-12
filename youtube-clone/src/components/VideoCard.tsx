9baa20e02865fd17a68b8f38128d87a980711288
rahulsainlll <rahulsain.social777@gmail.com>
2024-03-12 23:16:47 +0530

    Components

---
+++ youtube-clone/src/components/VideoCard.tsx
@@ -0,0 +1,10 @@
+export function VideoCard() {
+  return (
+    <div>
+      <img src="/Photo.png" alt="IMG" />
+    </div>
+  );
+}
+
+
+
--- youtube-clone/src/pages/index.tsx
+++ youtube-clone/src/pages/index.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { VideoCard } from "../components/VideoCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <div>
-    yop
-   </div>
+    <div>
+      <VideoCard />
+    </div>
   );
 }
+
+
+// I spent a day researching about the 2024 coding job market
+
